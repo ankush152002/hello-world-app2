16e7800839d4a9882e19a88be8b37ea8523f19d8
Ankush Singh <98583838+ankush152002@users.noreply.github.com>
2024-10-17 11:10:57 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from App 2!');
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
+
